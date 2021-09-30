936462809471d6398eccf3355e40e53571453f00
Gia Huy <nguyengiahuy16@gmail.com>
2021-09-30 12:11:34 +0700

    base server

--- tool-manager/src/main/session.ts
+++ tool-manager/src/main/session.ts
@@ -3,12 +3,13 @@ import { WebDriver } from "selenium-webdriver";
 export default class Session {
   driver!: WebDriver;
 
-  private constructor() {
+  private constructor(
+    private userId: string
+  ) {
   }
 
-
   static async build(userId: string) {
-    const session = new Session();
+    const session = new Session(userId);
     if (await session.init()) {
       return session;
     }
@@ -20,5 +21,8 @@ export default class Session {
   }
 
   private async downloadDataProfile() {
+  }
+
+  private async uploadDataProfile() {
   }
 }
