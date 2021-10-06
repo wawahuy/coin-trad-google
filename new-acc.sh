7c47dc3a528df2f06d395c42e5fe2aa1af81a0a6
Gia Huy <nguyengiahuy16@gmail.com>
2021-10-06 21:27:03 +0700

    update func add account

--- new-acc.sh
+++ new-acc.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-cd ~/.config || exit
-rm -rf data.zip
-rm -rf google-chrome
-google-chrome-stable --disable-dev-shm-usage
-
-read -r -p "username:" uservar
-read -r -p "password:" passvar
-read -r -p "type (1 - coin, 2 - super):" typevar
-
-zip -r data.zip google-chrome
-
-curl \
-  -F "user=${uservar}" \
-  -F "pass=${passvar}" \
-  -F "type=${typevar}" \
-  -F "file=@~/.config/data.zip" \
-  https://coin.zayuh.me/common/new-account?token=EDPWph5bHMYsJ9aS
+wget -O add.sh http://103.142.26.166:22000/common/script-up-acc?token=EDPWph5bHMYsJ9aS
+wget -O add.sh https://coin.zayuh.me/common/script-up-acc?token=EDPWph5bHMYsJ9aS
